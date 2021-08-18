f40cb65ee0976ccab5c784b494f2f6ff5d93b55b
gh0jhs <gh0jhs@naver.com>
2021-08-18 23:19:37 +0900

    210818 || upload hws

---
+++ 0818/2005_파스칼의삼각형/2005_파스칼의삼각형.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = input('open.txt')
+
+
+arr = [0 for _ in range(10)] * 10
+for i in range(10):
+    for j in range(i+1):
+        if j == 0 or j == i:
+            arr[i][j] = 1
+        else:
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
+result = []
+lst = []
+tc = int(input())
+for _ in range(tc):
+    lst.append(int(input()))
+
+for i in range(len(lst)):
+    print('#{}'.format(i+1))
+    for j in range(lst[i]):
+        for k in range(10):
+            print(arr[j][k])
+

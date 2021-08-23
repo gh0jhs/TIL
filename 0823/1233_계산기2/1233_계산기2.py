74de027ff5c40a2a597f9fd414152ba72bacf40b
gh0jhs <gh0jhs@naver.com>
2021-08-23 19:16:16 +0900

    210823 || upload hws

---
+++ 0823/1233_계산기2/1233_계산기2.py
@@ -0,0 +1,53 @@
+import sys
+sys.stdin = open('input.txt')
+
+for tc in range(1, 11):
+    n = int(input())
+    word = input()
+    A = '+*'
+    result = ''
+    stack = []
+    top = -1
+    for i in word:
+        if i not in A:
+            result += i
+        else:
+            if top == -1:
+                stack.append(i)
+                top += 1
+            elif i == '+':
+                while top != -1:
+                    result += stack.pop()
+                    top -= 1
+                stack.append(i)
+                top += 1
+            else:
+                if stack[top] == '+':
+                    stack.append(i)
+                    top += 1
+                else:
+                    while stack[top] == '+':
+                        result += stack.pop()
+                        top -= 1
+                        if top == -1:
+                            break
+                    stack.append(i)
+                    top += 1
+    while top != -1:
+        result += stack.pop()
+        top -= 1
+    stack = []
+    top = -1
+    for i in result:
+        if i not in A:
+            stack.append(int(i))
+        else:
+            if i == '+':
+                num2 = stack.pop()
+                num1 = stack.pop()
+                stack.append(num1 + num2)
+            elif i == '*':
+                num2 = stack.pop()
+                num1 = stack.pop()
+                stack.append(num1 * num2)
+    print('#{} {}'.format(tc, stack[0]))

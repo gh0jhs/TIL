b96cfc2e4aaddb5aa6516518d65efc8f3ae68ee4
gh0jhs <gh0jhs@naver.com>
2021-08-24 21:38:17 +0900

    210804 || upload hws

---
+++ 0824/4874_Forth/4874_Forth.py
@@ -0,0 +1,46 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+#### 9 / 10 fail
+
+T = int(input())
+A = '+-*/.'
+for tc in range(1, T+1):
+    stack = []
+    top = -1
+    arr = list(input().split())
+    for i in range(len(arr)):
+        if arr[i] not in A:
+            stack.append(int(arr[i]))
+            top += 1
+        else:
+            if top >= 1:
+                num2 = stack.pop()
+                top -= 1
+                num1 = stack.pop()
+                top -= 1
+                if arr[i] == '*':
+                    x = num1 * num2
+                    stack.append(int(x))
+                    top += 1
+                elif arr[i] == '/':
+                    x = num1 / num2
+                    stack.append(int(x))
+                    top += 1
+                elif arr[i] == '+':
+                    x = num1 + num2
+                    stack.append(int(x))
+                    top += 1
+                elif arr[i] == '-':
+                    x = num1 - num2
+                    stack.append(int(x))
+                    top += 1
+            else:
+                if arr[i] == '.':
+                    result = stack.pop()
+                    top -=1
+                else:
+                    result = 'error'
+                    break
+    print('#{} {}'.format(tc, result))
---
+++ 0824/4875_미로/4875_미로.py
@@ -0,0 +1,48 @@
+import sys
+sys.stdin = open('input.txt')
+
+# fail
+def dfs(x, y):
+    global visited
+    visited[x][y] = 1
+    # 우 하 좌 상
+    dx = [1, 0, -1 ,0]
+    dy = [0, -1, 0, 1]
+    for i in range(4):
+        if x == 0:
+            if i == 2:
+                continue
+        elif x == n-1:
+            if i == 0:
+                continue
+        if y == 0:
+            if i == 3:
+                continue
+        elif y == n-1:
+            if i == 1:
+                continue
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (arr[nx][ny] == 0 or arr[nx][ny] == 3) and visited[nx][ny] == 0:
+            dfs(nx, ny)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    s = (0, 0)
+    n = int(input())
+    arr = [list(map(int, (map(str, input())))) for _ in range(n)]
+    visited = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] == 2:
+                s = (i, j)
+            if arr[i][j] == 3:
+                e = (i, j)
+    dfs(s[0], s[1])
+    if visited[e[0]][e[1]] == 1:
+        result = 1
+    else:
+        result = 0
+    print(visited)
+    print('#{} {}'.format(tc, result))
